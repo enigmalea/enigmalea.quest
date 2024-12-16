2f77b268f18b6a891e1605dbd16174dc9bb18636
enigmalea <enigmalea@gmail.com>
2024-12-16 16:21:27 -0500

    added title component.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Unica One"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [require("@catppuccin/tailwindcss")({
-		defaultFlavour: "macchiato",
-	}),],
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      defaultFlavour: "macchiato",
+    }),
+  ],
 };
