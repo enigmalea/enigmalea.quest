044400786efe5f7bd85b03fea0f58462600a943e
enigmalea <enigmalea@gmail.com>
2024-12-28 22:59:32 -0500

    adds blog archive, tags, and categories pages.

--- src/utils/blog-utils.ts
+++ src/utils/blog-utils.ts
@@ -30,9 +30,59 @@ export async function getUpdates(): Promise<
     return dateA > dateB ? -1 : 1;
   });
 
-
-
-	const updates = sorted.slice(0,3);
+  const updates = sorted.slice(0, 3);
 
   return updates;
+}
+
+export type Tag = {
+  name: string;
+  count: number;
+};
+
+export async function getTagList(): Promise<Tag[]> {
+  const allBlogPosts = await getCollection<"posts">("posts", ({ data }) => {
+    return import.meta.env.PROD ? data.draft !== true : true;
+  });
+
+  const countMap: { [key: string]: number } = {};
+  allBlogPosts.map((post: { data: { tags: string[] } }) => {
+    post.data.tags.map((tag: string) => {
+      if (!countMap[tag]) countMap[tag] = 0;
+      countMap[tag]++;
+    });
+  });
+
+  // sort tags
+  const keys: string[] = Object.keys(countMap).sort((a, b) => {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  });
+
+  return keys.map((key) => ({ name: key, count: countMap[key] }));
+}
+
+export type Category = {
+  name: string;
+  count: number;
+};
+
+export async function getCategoryList(): Promise<Tag[]> {
+  const allBlogPosts = await getCollection<"posts">("posts", ({ data }) => {
+    return import.meta.env.PROD ? data.draft !== true : true;
+  });
+
+  const countMap: { [key: string]: number } = {};
+  allBlogPosts.map((post: { data: { category: string[] } }) => {
+    post.data.category.map((cat: string) => {
+      if (!countMap[cat]) countMap[cat] = 0;
+      countMap[cat]++;
+    });
+  });
+
+  // sort categories
+  const keys: string[] = Object.keys(countMap).sort((a, b) => {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  });
+
+  return keys.map((key) => ({ name: key, count: countMap[key] }));
 }
